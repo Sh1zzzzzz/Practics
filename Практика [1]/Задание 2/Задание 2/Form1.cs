872eaec70fe8ad1bccd953c6b2ee329849804799
L-Il <0rky@pm.me>
2021-03-23 23:39:37 +0300

    + First 2 practics.

---
+++ Практика [1]/Задание 2/Задание 2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.GreenYellow;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Начало работы";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "До свидания!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Привет!";
+        }
+    }
+}
