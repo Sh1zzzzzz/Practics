6e9235b48efed257f436e9eb80026be2f685f34b
L-Il <0rky@pm.me>
2021-03-24 01:41:04 +0300

    + Задание 1

---
+++ Практика [2]/Задание 1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Задание_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите Х:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 43);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат выполнения программы:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 52);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 136);
+            this.textBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {"5", "8", "11", "14", "2"});
+            this.comboBox1.Location = new System.Drawing.Point(165, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(31, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 20);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 226);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ Практика [2]/Задание 1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "Практическая работа №1 Чурсинов Г.С.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+      textBox2.Text += Environment.NewLine + "При x = " + x.ToString();
+      double y;
+      switch (comboBox1.Text)
+      {
+        case "5":
+          y = (x + Math.Sin(0.5 * x) + Math.Sqrt(Math.Log(x, 10)) / (2 + 3 * Math.Cos(x)) + (Math.Pow(Math.E, 2 * x - 0.5)) / (2 + x));
+          textBox2.Text += Environment.NewLine + "№ 5 Результат y = " + y.ToString();
+          break;
+        case "8":
+          y = (3 * (x * x - Math.Abs(3 * x) + Math.Abs(1 - Math.Cos(x * x)))) / (2 * Math.Pow(Math.E, 3 * x - 2) - 3 * ((2 + x) / x));
+          textBox2.Text += Environment.NewLine + "№ 8 Результат y = " + y.ToString();
+          break;
+        case "11":
+          y = (Math.Sqrt(3 + Math.Log(x, 10) + 15 - x)) / (1 + Math.Sin((2 + x * x) / (1 + x)));
+          textBox2.Text += Environment.NewLine + "№11 Результат y = " + y.ToString();
+          break;
+        case "14":
+          y = (Math.Sqrt(2 + 0.5 * x * x + Math.Pow(Math.E, x + 2))) / (x * x * x - 3 * Math.Pow(2 * x - 1, 2));
+          textBox2.Text += Environment.NewLine + "№14 Результат y = " + y.ToString();
+          break;
+        case "2":
+          y = (0.5 * x * x - 1) / (Math.Sqrt(x)) + (Math.Abs(10 - Math.Pow(Math.E, 0.5 * x * x + 1))) / (Math.Log(2 * x, 10) - 1);
+          textBox2.Text += Environment.NewLine + "№ 2 Результат y = " + y.ToString();
+          break;
+        }
+
+      // 5  = (x + Math.Sin(0.5 * x) + Math.Sqrt(Math.Log(x, 10)) / (2 + 3 * Math.Cos(x)) + (Math.Pow(Math.E, 2 * x - 0.5)) / (2 + x));
+      // 8  = (3 * (x * x - Math.Abs(3 * x) + Math.Abs(1 - Math.Cos(x * x)))) / (2 * Math.Pow(Math.E, 3 * x - 2) - 3 * ((2 + x) / x));
+      // 11 = (Math.Sqrt(3 + Math.Log(x, 10) + 15 - x)) / (1 + Math.Sin((2 + x * x) / (1 + x)));
+      // 14 = (Math.Sqrt(2 + 0.5 * x * x + Math.Pow(Math.E, x + 2))) / (x * x * x - 3 * Math.Pow(2 * x - 1, 2));
+      // 2  = (0.5 * x * x - 1) / (Math.Sqrt(x)) + (Math.Abs(10 - Math.Pow(Math.E, 0.5 * x * x + 1))) / (Math.Log(2 * x, 10) - 1);
+        }
+    }
+}
