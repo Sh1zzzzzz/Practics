d65d5b6ddd91c80a4342ee11584ec7dc3c5dd33b
L-Il <0rky@pm.me>
2021-03-29 18:00:28 +0300

    + Задание 11

---
+++ Практика [5]/Задание 11/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_11
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+      int index = listBox1.SelectedIndex;
+      string str = (string)listBox1.Items[index];
+      int len = str.Length;
+      int count = 0;
+      int i = 0;
+      while (i < len)
+      {
+
+        if (str[i] == '0' ^ str[i] == '1' ^ str[i] == '2' ^ str[i] == '3' ^ str[i] == '4' ^ str[i] == '5' ^ str[i] == '6' ^ str[i] == '7' ^ str[i] == '8' ^ str[i] == '9' )
+          count++;
+        i++;
+      }
+      label1.Text = "Количество цифр = " + count.ToString();
+    }
+    }
+}
