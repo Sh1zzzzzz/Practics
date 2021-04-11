346b359ff331bd23a4f38aa72013657c9c575e1b
l-Il <0rky@pm.me>
2021-04-11 16:32:49 +0300

    + Задание 1

--- Практика [9]/Задание 1/Form1.Designer.cs
+++ Практика [9]/Задание 1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Задание_11
+﻿namespace Задание_1
 {
     partial class Form1
     {
@@ -35,10 +35,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 361);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Задание 1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
         }
 
---
+++ Практика [9]/Задание 1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            // g.DrawLine(pen, 0, 300, 600, 300);
+            g.FillRectangle(new SolidBrush(Color.SkyBlue), 0, 0, 600, 300);
+            g.FillEllipse(new SolidBrush(Color.Yellow), 20, 10, 40, 40);
+            g.FillRectangle(new SolidBrush(Color.ForestGreen), 0, 300, 600, 100);
+            g.FillRectangle(new SolidBrush(Color.Brown), 200, 200, 100, 100);
+            g.FillRectangle(new SolidBrush(Color.CadetBlue), 225, 225, 50, 50);
+            Point[] curvePoints = {new Point(165, 200), new Point(250, 150), new Point(335, 200)};
+            g.FillPolygon(new SolidBrush(Color.SaddleBrown), curvePoints);
+            g.FillRectangle(new SolidBrush(Color.SaddleBrown), 450, 225, 20, 100);
+            g.FillEllipse(new SolidBrush(Color.Green), 425, 190, 70, 70);
+        }
+    }
+}
